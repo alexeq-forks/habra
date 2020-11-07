fb6d51cb1b66f5b8310bcdac09fad60c973e03b4
jarvis394 <jarvis394@users.noreply.github.com>
2020-11-07 12:47:59 +0000

    deployed via Github Pages Deploy Action 🚀 for a4a19529609a56149439b83e684ba22b5b7cbd81

---
+++ precache-manifest.277abb87e8a444f40ac6a959d6dece47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9db4c63006a7044d749ca7e5b8d49b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "323f0ba3fcc48002eb06",
+    "url": "/static/css/2.80e028c5.chunk.css"
+  },
+  {
+    "revision": "323f0ba3fcc48002eb06",
+    "url": "/static/js/2.89edafa0.chunk.js"
+  },
+  {
+    "revision": "4262fbd88ef667a896128fa7f032257f",
+    "url": "/static/js/2.89edafa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccff6d0b1688f3266071",
+    "url": "/static/js/main.2727594c.chunk.js"
+  },
+  {
+    "revision": "fe80b5c065de840298e8",
+    "url": "/static/js/runtime-main.c05885d4.js"
+  }
+]);
